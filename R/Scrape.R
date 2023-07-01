7ee174cdf607473c0256de86089a208624edc093
kzktmr <kzktmr@users.noreply.github.com>
2023-07-02 00:06:36 +0900

    First Step

--- R/Scrape.R
+++ R/Scrape.R
@@ -3,7 +3,6 @@ library(tidyr)
 library(readr)
 library(stringr)
 library(rvest)
-library(ggplot2)
 
 region <- tibble(
   code = c("00", "08", "09", "10", "11", "12", "13", "14", "15", "42", 
@@ -52,7 +51,7 @@ for(i in 1:19){
 }
 tmp <- tmp |> bind_rows()
 
-tmp
+# tmp
 
 dat_updated <- dat |> 
   left_join(tmp, by = join_by(year, week, name, region)) |> 
@@ -67,29 +66,3 @@ dat <- dat_updated |> bind_rows(tmp_filterd)
 today <-Sys.Date()
 
 dat |> write_excel_csv(paste("data/data_", today, ".csv", sep = ""))
-
-dat |> filter(name == "per_sentinel", region == "福岡県") |> 
-  mutate(year_week = str_glue("{year}_{sprintf('%02d', week)}")) |> 
-  ggplot() + aes(x = year_week, y = value) + geom_col() + 
-  theme_bw() + labs(title = "福岡県新型コロナウイルス感染症定点当り報告数") +
-  theme(plot.title = element_text(family = "筑紫A丸ゴシック"),
-        axis.title = element_blank(),
-        axis.text.x = element_text(angle = 90, vjust = 0.5))
-
-ggsave(paste("figure/figure_", today, ".png"), width = 1920, height = 1080, units = "px",
-       dpi = "retina")
-
-dat |> filter(name == "per_sentinel", region == "福岡県") |> 
-  mutate(year_week = str_glue("{year}_{sprintf('%02d', week)}"),
-         growth = value / lag(value)) |> drop_na() |> 
-  ggplot() + aes(x = year_week, y = growth) + geom_line(aes(group = region), color = "navy") + 
-  geom_hline(yintercept = 1, color = "red", linetype = "dashed") +
-  theme_bw() + labs(title = "福岡県新型コロナウイルス感染症定点当り報告数前週比") +
-  scale_y_log10() +
-  theme(plot.title = element_text(family = "筑紫A丸ゴシック"),
-        axis.title = element_blank(),
-        axis.text.x = element_text(angle = 90, vjust = 0.5))
-
-ggsave(paste("figure/growth_", today, ".png"), width = 1920, height = 1080, units = "px",
-       dpi = "retina")
-  
